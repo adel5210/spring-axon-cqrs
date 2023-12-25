1d406f6d44dbcc9efd9fc3390fd911dae1743412
adel <adel_5210m@yahoo.com>
2023-12-26 01:26:26 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Axon + Spring (CQRS and Event Sourcing)
+
+### Requirements
+- Axon Server (https://download.axoniq.io/axonserver/AxonServer.zip)
---
+++ src/test/java/com/adel/springaxon/SpringAxonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adel.springaxon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAxonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
